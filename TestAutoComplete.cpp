6e6fe004b8b67bcbba6ee4f550808865c40321e5
VampireTeeth <vampire001@gmail.com>
2013-01-25 19:13:49 +1100

    TestAutoComplete added

---
+++ TestAutoComplete.cpp
@@ -0,0 +1,43 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  TestAutoComplete.cpp
+ *
+ *    Description:  Test AutoComplete
+ *
+ *        Version:  1.0
+ *        Created:  25/01/2013 19:02:41
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Steven
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+using namespace std;
+
+class Person{
+  const char* Name;
+  int Age;
+public:
+  Person(const char* Name="", int Age=0);
+  const char* name() const;
+  int age() const;
+};
+
+Person::Person(const char* Name, int Age):Name(Name), Age(Age){}
+inline const char* Person::name() const{
+  return Name;
+}
+
+inline int Person::age() const {
+  return Age;
+}
+
+int main(){
+  Person* p = new Person("Steven", 28);
+  cout << p->age() << ", " << p->name() << endl;
+}
+
